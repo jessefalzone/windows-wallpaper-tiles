3a5b9aa11628b502046f611e1c7a1adadcbff7be
Jesse Falzone <allow.hams-06@icloud.com>
2024-06-25 08:43:48 -0400

    Added CSS hashes, robots.txt, and headers.

--- src/generate.js
+++ src/generate.js
@@ -4,6 +4,7 @@ import fs from 'fs/promises';
 import Handlebars from 'handlebars';
 import { minify } from 'html-minifier-terser';
 import CleanCSS from 'clean-css';
+import revisionHash from 'rev-hash';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -58,11 +59,11 @@ async function getAllFilesAndDirectories(dirPath) {
 }
 
 async function generateCSS(files) {
-  let cssContent = '';
+  let backgroundCss = '';
 
   for (let file of files) {
     for (let content of file.contents) {
-      cssContent += `
+      backgroundCss += `
         body:has(select option[value="${content.value}"]:checked) {
           background-image: url("${content.path}");
         }
@@ -72,29 +73,36 @@ async function generateCSS(files) {
   const options = {
     level: 2,
   };
-  cssContent = new CleanCSS(options).minify(cssContent).styles;
-  await fs.writeFile(OUTPUT_BACKGROUND_CSS, cssContent);
+  backgroundCss = new CleanCSS(options).minify(backgroundCss).styles;
+  const backgroundsHash = revisionHash(backgroundCss);
+  await fs.writeFile(OUTPUT_BACKGROUND_CSS, backgroundCss);
 
-  const mainCss = await fs.readFile(INPUT_MAIN_CSS);
-  await fs.writeFile(
-    OUTPUT_MAIN_CSS,
-    new CleanCSS(options).minify(mainCss).styles
-  );
+  let mainCss = await fs.readFile(INPUT_MAIN_CSS);
+  mainCss = new CleanCSS(options).minify(mainCss).styles;
+
+  const mainHash = revisionHash(mainCss);
+  await fs.writeFile(OUTPUT_MAIN_CSS, mainCss);
+
+  return {
+    mainHash,
+    backgroundsHash,
+  };
 }
 
 (async function main() {
   const files = await getAllFilesAndDirectories(IMG_DIR);
 
+  const { mainHash, backgroundsHash } = await generateCSS(files);
+
   const templateString = await fs.readFile(TEMPLATE);
   const template = Handlebars.compile(templateString.toString());
-  const html = await minify(template({ files }), {
+  const html = await minify(template({ files, mainHash, backgroundsHash }), {
     minifyJS: true,
     collapseWhitespace: true,
     removeComments: true,
   });
 
   await fs.writeFile(OUTPUT_HTML, html);
-  await generateCSS(files);
 
   console.log('Done!');
 })();
